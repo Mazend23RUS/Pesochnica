0a83f4553678fa721cd0b2644a8b1a6c91b34ed3
Алексей <bboy23@mail.ru>
2023-08-12 17:55:19 +0300

    Реализация записи/чтения бинарного файла

---
+++ src/main/java/org/example/Example.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Example {
+
+    String testText;
+    int testInt;
+    double testDouble;
+
+
+    Example(){}
+
+    public Example(String s, int i, double d){
+        testText = s;
+        testInt = i;
+        testDouble = d;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import javafx.scene.shape.Path;
+
 import javax.xml.transform.Source;
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -62,16 +66,92 @@ public class Main {
 //        System.out.println(result);
 
 
-       /** Реализация через BufferdReader */
+//       /** Реализация через BufferdReader, стрим */
+//
+//       BufferedReader br = new BufferedReader(new FileReader(new File("/Програмирование/Test_file.txt")));
+//       br.lines().forEach(System.out::println);
 
-       BufferedReader br = new BufferedReader(new FileReader(new File("/Програмирование/Test_file.txt")));
-       br.lines().forEach(System.out::println);
+        /** Реализация через BufferdReader */
 
 
+//        BufferedReader br = new BufferedReader(new FileInputStream("/Програмирование/Test_file.txt"));
+//        try(FileInputStream fir = new FileInputStream("/Програмирование/Test_file.txt")){
+//        ArrayList<Byte> resultInt = new ArrayList<>();
+//        String fromFile ="";
+//
+//        while (fir.read() != -1){
+//            resultInt.add((byte)(fir.read()));
+//        }
+//        byte [] bytesArray = new byte[resultInt.size()];
+//            for (int i = 0; i < bytesArray.length-1; i++) {
+//                bytesArray[i] = resultInt.get(i);
+//                System.out.println(bytesArray[i]);
+//            }
 
+
+//            ByteBuffer b = ByteBuffer.allocate(resultInt.size());
+//        String st = new String(bytesArray,StandardCharsets.UTF_8);
+//            char c = (char) bytesArray[1];
+//            System.out.println(c);
+
+//            int a = 181;
+//            byte bt = (byte) a;
+
+//            System.out.println(string);
+//            System.out.println(charResult);
+//        ArrayList<Character> characters = resultInt.stream().map(m ->).collect(Collectors.toCollection(ArrayList::new));
+//            System.out.println(resultInt);
+//            System.out.println(st);
+
+        /** Запись в файл байт кода */
+
+//            try (FileOutputStream fos = new FileOutputStream(new File("/Програмирование/Test_file.txt"));
+//            DataOutputStream dos = new DataOutputStream(fos)){
+//
+//
+//
+//                double testDouble = 156.15;
+//                String testText = "Какая то фигня для текста";
+//                int testInt = 894616;
+//
+//                dos.writeDouble(testDouble);
+//                dos.writeChars(testText);
+//                dos.writeInt(testInt);
+//                System.out.println("Файл записан в бинарном варианте");
+//
+//
+//            }
+
+
+        Long aLong;
+        String aString;
+        Double aDouble;
+
+
+        try (FileInputStream fis = new FileInputStream(new File("Result_test_binaryFile.bin"));
+             DataInputStream ois = new DataInputStream(fis)) {
+
+            aLong = ois.readLong();
+            System.out.println(aLong);
+
+            aDouble = ois.readDouble();
+            System.out.println(aDouble);
+
+            while (true) {
+                aString = String.valueOf(ois.readChar());
+                System.out.print(aString);
+            }
+
+        } catch (EOFException e) {
+            // Оставляем пустым , что бы не отлавливал ошибку при когда чтение файла закончилось
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
 
 
 }
+
+
